452e4959c30373cad0502994f627e178d245168a
Renish Radadiya <renish.megaminds@gmail.com>
2023-03-29 12:42:42 +0530

    initial commite

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.get("/", (req, res) => res.type("html").send(html));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
 
 const html = `
 <!DOCTYPE html>
@@ -56,4 +55,4 @@ const html = `
     </section>
   </body>
 </html>
-`
+`;
